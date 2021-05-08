e48edb171dda10ba9418d30e402c25dbed09a623
”kensuke” <kensuke1202left@icloud.com>
2021-05-08 23:30:15 +0900

    settingページにuser情報を表示

--- mytrainer-app-laravel/resources/views/setting.blade.php
+++ mytrainer-app-laravel/resources/views/setting.blade.php
@@ -7,13 +7,19 @@
                 <p>Edit profile</p>
                 <form action="/setting?param=name" method="POST">
                     @csrf
-                    <label for="name-label">Name</label><br>
+                    <label for="name-label">User name : {{ Auth::user()->name }}</label><br>
                     <input type="text" name="name" id="name-label">
                     <button type="submit">setting</button>
                 </form>
                 <form action="/setting?param=email" method="POST">
                     @csrf
-                    <label for="name-label">E-mail</label><br>
+                    <label for="name-label">E-mail : 
+                        @if(Auth::user()->email == null)
+                            設定されていません
+                        @else
+                            {{ Auth::user()->email }}
+                        @endif
+                    </label><br>
                     <input type="text" name="email" id="email-label">
                     <button type="submit">setting</button>
                 </form>
