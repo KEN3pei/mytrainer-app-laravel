c141681df194b29df040f4080acaf447c734f417
”kensuke” <kensuke1202left@icloud.com>
2021-05-09 02:45:56 +0900

    各ページのレスポンシブ対応

--- mytrainer-app-laravel/resources/views/top.blade.php
+++ mytrainer-app-laravel/resources/views/top.blade.php
@@ -5,5 +5,13 @@
             <a href="">＋</a>
             <p>トレーニングメニューを作成</p>
         </div>
+        <div class="sp-menuLists">
+            @foreach($lists as $list)
+                <!-- <span class="three-point">︙<span class="delete-popup" id="{{ $list->list_id }}">削除</span></span> -->
+                <p><a href="/top/menulist?listname={{ $list->list_name }}">{{ $list->list_name }}</a>
+                <span class="three-point">︙<span class="delete-popup" id="{{ $list->list_id }}">削除</span></span>
+                </p>
+            @endforeach
+        </div>
     </div>
 @endsection
