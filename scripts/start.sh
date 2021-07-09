039e3e0b0bffa7110a586f93ddd599c74bca1b7a
”kensuke” <kensuke1202left@icloud.com>
2021-07-09 17:22:18 +0900

    docker demon 起動コマンドを追加

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,6 +4,7 @@ sudo yum update -y
 echo y | sudo yum install docker
 echo y | sudo curl -L https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+sudo service docker start
 
 docker network create ALLHOME-PROJECT-NETWORK
 
