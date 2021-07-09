47cc70c125020764a6fbfdac78fc487f71e3db82
”kensuke” <kensuke1202left@icloud.com>
2021-07-09 17:03:21 +0900

    dockerのinstallコマンドなどを追加

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+sudo yum update -y
+sudo yum install docker
+sudo curl -L https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
 docker network create ALLHOME-PROJECT-NETWORK
 
 docker-compose -p mytrainer-app-application-server -f /var/app/docker-compose.yml down
