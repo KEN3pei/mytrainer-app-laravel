9db7c7c4dacc991d814d55b360943e742bd69498
”kensuke” <kensuke1202left@icloud.com>
2021-07-11 12:45:51 +0900

    接続先IPアドレスを変更

--- mytrainer-app-laravel/config/database.php
+++ mytrainer-app-laravel/config/database.php
@@ -46,7 +46,8 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', '172.31.43.71'), // 接続先インスタンスprivateip
+            // 'host' => env('DB_HOST', '127.0.0.1'), デフォルト設定
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
