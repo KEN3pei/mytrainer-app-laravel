40d5a6ab02960bdc112c6d5ed9daf2bca726acee
kensukeUeno <kensuke1202left@icloud.com>
2022-04-26 01:58:47 +0900

    newQuery関数を調べるための様々な修正

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # mytrainer-app-laravel
+
+# start container 
+`docker compose up -d`
+
+# cp create .env
+`cp .env.example .env`
+
+# install Laravel packages
+`composer install`
+
+# migration
+`php artisan migrate`
+
+# generate appKey 
+`php artisan key:generate`
+
+# setting vscode
+* ineffective package below vendor  
+command Palette -> user setting -> gitignore -> checkout "User gitignore files"
+
+* php Intelephense ineffective  
+downgrade to 1.2.3
--- mytrainer-app-laravel/app/DataProvider/TrainingMenuItemRepository.php
+++ mytrainer-app-laravel/app/DataProvider/TrainingMenuItemRepository.php
@@ -8,17 +8,33 @@ class TrainingMenuItemRepository implements TrainingMenuItemRepositoryInterface
 {
     private $training_menu_item;
 
+    /**
+     * Undocumented function
+     *
+     * @param TrainingMenuItem $training_menu_item
+     */
     public function __construct(TrainingMenuItem $training_menu_item)
     {
         $this->training_menu_item = $training_menu_item;
     }
 
+    /**
+     * Undocumented function
+     *
+     * @param string $keyword
+     * @return void
+     */
     public function search(string $keyword)
     {
-        $items = $this->training_menu_item->where('item_name', 'like', "%$keyword%")->get();
+        $items = $this->training_menu_item->newQuery()->where('item_name', 'like', "%$keyword%")->get();
         return $items;
     }
 
+    /**
+     * Undocumented function
+     *
+     * @return void
+     */
     public function getAllItem(){
         $all_item = $this->training_menu_item->get();
         return $all_item;
